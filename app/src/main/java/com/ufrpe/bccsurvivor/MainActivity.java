0a50db6f4f76c5e1a23944855761eeb9e81acb5e
Daniel Freire <danjfreire@gmail.com>
2017-11-16 15:17:44 -0200

    teste

--- app/src/main/java/com/ufrpe/bccsurvivor/MainActivity.java
+++ app/src/main/java/com/ufrpe/bccsurvivor/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference myRef = database.getReference("Testando");
 
+                myRef.setValue("wallace");
             }
         });
 
