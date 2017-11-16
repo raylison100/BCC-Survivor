9c9a5eebc3047d14811d6d0442393b775a4161cb
Daniel Freire <danjfreire@gmail.com>
2017-11-16 11:48:51 -0200

    teste banco 1

--- app/src/main/java/com/ufrpe/bccsurvivor/MainActivity.java
+++ app/src/main/java/com/ufrpe/bccsurvivor/MainActivity.java
@@ -2,6 +2,11 @@ package com.ufrpe.bccsurvivor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btnTeste = (Button)findViewById(R.id.btnTeste);
+        btnTeste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("Testando");
+
+            }
+        });
+
     }
 }
