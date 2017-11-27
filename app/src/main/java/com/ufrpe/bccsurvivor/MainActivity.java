5b115d57ef844594a31007ce90b57ed2335579c2
raylison100 <Ray99291072*>
2017-11-27 14:46:42 -0300

    Novas telas

--- app/src/main/java/com/ufrpe/bccsurvivor/MainActivity.java
+++ app/src/main/java/com/ufrpe/bccsurvivor/MainActivity.java
@@ -2,8 +2,7 @@ package com.ufrpe.bccsurvivor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,15 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        Button btnTeste = (Button)findViewById(R.id.btnTeste);
-        btnTeste.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
     }
 }
